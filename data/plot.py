bff552201253ac5fdb1f6179000c1cb35fa54f50
Haylay Tsegab <131392102+HaylayTsegab@users.noreply.github.com>
2023-05-27 11:31:49 +0000

    simple plot using seaborn

---
+++ data/plot.py
@@ -0,0 +1,17 @@
+#importing required libraries for data visualization
+import matplotlib.pyplot as plt
+import seaborn as sns 
+import numpy as np
+import pandas as pd
+# data name from the seaborn library
+data = sns.load_dataset('tips')
+
+#examining the fist five rows of the data
+print(data.head())
+
+#looking if there are missing valuse in the dataset
+sns.heatmap(data.isnull(),yticklabels=False,cbar=False,cmap='viridis')
+#plotting the data
+sns.barplot(x='day',y='total_bill',data=data)
+plt.show()
+
